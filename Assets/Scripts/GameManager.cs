fa6a63ab69388257402614e6b9d3d06e4695a093
Kaminskyi <50293330+MarvisClause@users.noreply.github.com>
2021-08-24 14:29:17 +0300

    Added ending ia a draw

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,11 +11,14 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverPanel;
     public Text gameOverText;
 
+    private int moveCount;
+
     public void Awake()
     {
         gameOverPanel.SetActive(false);
         SetGameManagerReferenceOnButtons();
         playerSide = "X";
+        moveCount = 0;
     }
     void SetGameManagerReferenceOnButtons()
     {
@@ -31,6 +34,7 @@ public class GameManager : MonoBehaviour
     } 
     public void EndTurn()
     {
+        moveCount++;
         if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
         {
             GameOver();
@@ -63,6 +67,12 @@ public class GameManager : MonoBehaviour
         {
             GameOver();
         }
+
+        if (moveCount >= 9)
+        {
+           
+            SetGameOverText( "It`s a draw !");
+        }
         ChangeSides();
     } 
     void GameOver()
@@ -71,11 +81,16 @@ public class GameManager : MonoBehaviour
         {
             buttonList[i].GetComponentInParent<Button>().interactable = false;
         }
-        gameOverPanel.SetActive(true);
-        gameOverText.text = playerSide + " wins!";
+        SetGameOverText ( playerSide + " wins!");
     } 
     void ChangeSides()
     {
         playerSide = (playerSide == "X") ? "0" : "X";
+    } 
+    void SetGameOverText(string value)
+    {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = value;
     }
+         
 }
